fa4c7607c2995a0c7dd5caeeeb0e14105c4f48de
Khin Thwet Lay <khinthwetlay.mm@gmail.com>
2022-04-17 22:31:35 +0630

    add seederdatabase

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Blog;
+use App\Models\Category;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +18,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         User::truncate();
+         Category::truncate();
+         Blog::truncate();
+        \App\Models\User::factory()->create();
+       
+        $frontend=Category::create([
+            'name' => 'frontend',
+            'slug' => 'frontend'
+        ]);
+        $backend=Category::create([
+            'name' => 'backend',
+            'slug' => 'backend'
+        ]);
+        Blog::create([
+            'title' => 'frontend post',
+            'slug' => 'frontend-post',
+            'intro' => 'this is intro',
+            'body' => 'this is body',
+            'category_id' => $frontend->id
+        ]);
+        Blog::create([
+            'title' => 'backend post',
+            'slug' => 'backend-post',
+            'intro' => 'this is intro',
+            'body' => 'this is body',
+            'category_id' => $backend->id
+        ]);
     }
+    
 }
