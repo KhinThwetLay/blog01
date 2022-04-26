36235938b2c2728d9992149c58cc94938e326737
Khin Thwet Lay <khinthwetlay.mm@gmail.com>
2022-04-26 14:15:47 +0630

    add filter by category

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Blog;
 use App\Models\Category;
 use App\Models\User;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -27,7 +25,8 @@ Route::get('/', function () {
     //     logger($query->sql);//logger is same to log::info('affff');
     // });
     return view('blogs',[
-        "blogs" =>Blog::latest()->get()// Blog::with('category','author')->get()//with() is called lazay loading or eager loader
+        "blogs" =>Blog::latest()->get(),
+        "categories" =>Category::all()// Blog::with('category','author')->get()//with() is called lazay loading or eager loader
     ]);
 });
 Route::get('/blogs/{blog:slug}', function (Blog $blog) { // $blog=Blog::findOrFail($id);
@@ -41,13 +40,16 @@ Route::get('/blogs/{blog:slug}', function (Blog $blog) { // $blog=Blog::findOrFa
 
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('blogs',[
-        "blogs" => $category->blogs
+        "blogs" => $category->blogs,
+         "categories" =>Category::all(),
+         "currentCategory" => $category
     ]);
 });
 
 Route::get('/users/{user:username}', function (User $user) {
 
     return view('blogs',[
-        "blogs" => $user->blogs
+        "blogs" => $user->blogs,
+         "categories" =>Category::all()
     ]);
 });
