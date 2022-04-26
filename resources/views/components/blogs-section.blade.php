b15194e2bb3253bd776d5fd20786768744c5b140
Khin Thwet Lay <khinthwetlay.mm@gmail.com>
2022-04-26 22:37:31 +0630

    add blogs not found code in loop

--- resources/views/components/blogs-section.blade.php
+++ resources/views/components/blogs-section.blade.php
@@ -36,10 +36,14 @@
         </div>
       </form>
       <div class="row">
-        @foreach($blogs as $blog)
+        @forelse($blogs as $blog)
             <div class="col-md-4 mb-4">
                 <x-blog-card  :blog="$blog"/>
             </div>
-        @endforeach
+            @empty
+            <p>Blogs Not Found</p>
+        @endforelse
+
+
       </div>
     </section>
